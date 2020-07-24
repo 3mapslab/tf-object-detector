4ef3a7ce98aa2a5000c48e9a2210bf0bc86bb32c
Licinio Carvalho <licane1213@hotmail.com>
2020-07-24 10:02:20 +0100

    ref #46

--- app/api.py
+++ app/api.py
@@ -45,7 +45,7 @@ def image():
         # Get the target object class to be detected (alternative: request.headers['target_class'])
         target_class = request.form.get('target_class')
 
-        # finally run the image through tensor flow object detection`
+        # finally run the image through tensor flow object detection
         image_object = Image.open(image_file)
         objects = object_detection_api.get_objects(image_object, target_class, threshold)
 
